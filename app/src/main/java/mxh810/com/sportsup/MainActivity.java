3e3b3dec47dabe63ed70f7a8ba9906903b2d80cb
JillMeng <76186275+JillMeng@users.noreply.github.com>
2021-11-21 23:38:53 -0800

    fixed package and added button in main activity

--- app/src/main/java/mxh810/com/sportsup/MainActivity.java
+++ app/src/main/java/mxh810/com/sportsup/MainActivity.java
@@ -2,7 +2,10 @@ package mxh810.com.sportsup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,9 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.home:
+                ChangeActivity();
+                break;
+        }
+    }
 
-
-
+    private void ChangeActivity() {
+        Intent intent1 = new Intent(MainActivity.this, Dashboard.class);
+        startActivity(intent1);
     }
 }
