3424626c6d2926c63fb9d0488273e9faa2c15cbb
JillMeng <76186275+JillMeng@users.noreply.github.com>
2021-12-08 17:57:21 -0800

    this is a draft version, please do not pull

--- app/src/main/java/mxh810/com/sportsup/YogaClass.java
+++ app/src/main/java/mxh810/com/sportsup/YogaClass.java
@@ -45,70 +45,70 @@ import java.util.List;
      protected void onCreate(Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_yoga_class);
+//
+//
+//         btnPlay1 = (Button) findViewById(R.id.play_button_1);
+//         mYouTubePlayerView1 = (YouTubePlayerView) findViewById(R.id.youtube_player_view_1);
+//         btnPlay2 = (Button) findViewById(R.id.play_button_2);
+//         mYouTubePlayerView2 = (YouTubePlayerView) findViewById(R.id.youtube_player_view_2);
+//         btnPlay3 = (Button) findViewById(R.id.play_button_3);
+//         mYouTubePlayerView3 = (YouTubePlayerView) findViewById(R.id.youtube_player_view_3);
+//
+//         Log.d(TAG, "onCreat: Starting...");
 
+//         btnPlay1.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//             public void onClick(View view) {
+////                 videoStr = "lDbJdJWX82M";
+//                 Log.d(TAG,"onClick: Initializing.");
+//                 mYouTubePlayerView1.initialize(YouTubeConfig.getApiKey(),mOnInitializedListener);
+//             }
+//         });
 
-         btnPlay1 = (Button) findViewById(R.id.play_button_1);
-         mYouTubePlayerView1 = (YouTubePlayerView) findViewById(R.id.youtube_player_view_1);
-         btnPlay2 = (Button) findViewById(R.id.play_button_2);
-         mYouTubePlayerView2 = (YouTubePlayerView) findViewById(R.id.youtube_player_view_2);
-         btnPlay3 = (Button) findViewById(R.id.play_button_3);
-         mYouTubePlayerView3 = (YouTubePlayerView) findViewById(R.id.youtube_player_view_3);
+//         mOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
+//             @Override
+//             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+//                 if(youTubePlayer == null) return;
+//                 if(b) {
+//                     youTubePlayer.play();
+//                 } else {
+//                     Log.d(TAG, "onClick: Done initializing.");
+//                     youTubePlayer.cueVideo(videoStr);
+//                 }
+//             }
+//
+//             @Override
+//             public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+//                 Log.d(TAG, "onClick: Failed to initialize.");
+//             }
+//         };
 
-         Log.d(TAG, "onCreat: Starting...");
-
-         btnPlay1.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View view) {
-                 videoStr = "lDbJdJWX82M";
-                 Log.d(TAG,"onClick: Initializing.");
-                 mYouTubePlayerView1.initialize(YouTubeConfig.getApiKey(),mOnInitializedListener);
-             }
-         });
-
-         mOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
-             @Override
-             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                 if(youTubePlayer == null) return;
-                 if(b) {
-                     youTubePlayer.play();
-                 } else {
-                     Log.d(TAG, "onClick: Done initializing.");
-                     youTubePlayer.loadVideo(videoStr);
-                 }
-             }
-
-             @Override
-             public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-                 Log.d(TAG, "onClick: Failed to initialize.");
-             }
-         };
-
-         btnPlay2.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View view) {
-                 videoStr = "lDbJdJWX82M";
-                 Log.d(TAG,"onClick: Initializing.");
-                 mYouTubePlayerView2.initialize(YouTubeConfig.getApiKey(),mOnInitializedListener2);
-             }
-         });
-
-         mOnInitializedListener2 = new YouTubePlayer.OnInitializedListener() {
-             @Override
-             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                 if(youTubePlayer == null) return;
-                 if(b) {
-                     youTubePlayer.play();
-                 } else {
-                     Log.d(TAG, "onClick: Done initializing.");
-                     youTubePlayer.loadVideo(videoStr);
-                 }
-             }
-
-             @Override
-             public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-                 Log.d(TAG, "onClick: Failed to initialize.");
-             }
-         };
+//         btnPlay2.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//             public void onClick(View view) {
+//                 videoStr = "lDbJdJWX82M";
+//                 Log.d(TAG,"onClick: Initializing.");
+//                 mYouTubePlayerView2.initialize(YouTubeConfig.getApiKey(),mOnInitializedListener2);
+//             }
+//         });
+//
+//         mOnInitializedListener2 = new YouTubePlayer.OnInitializedListener() {
+//             @Override
+//             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+//                 if(youTubePlayer == null) return;
+//                 if(b) {
+//                     youTubePlayer.play();
+//                 } else {
+//                     Log.d(TAG, "onClick: Done initializing.");
+//                     youTubePlayer.loadVideo(videoStr);
+//                 }
+//             }
+//
+//             @Override
+//             public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+//                 Log.d(TAG, "onClick: Failed to initialize.");
+//             }
+//         };
 
 
 //
@@ -144,6 +144,24 @@ import java.util.List;
 //             }
 //         });
 
-
      }
+
+//     public void onClick(View view) {
+//         switch(view.getId()) {
+//             case R.id.play_button_2:
+//                 videoStr = "TrgNZn0xejo";
+//                 Log.d(TAG,"onClick: Initializing.");
+//                 mYouTubePlayerView1.initialize(YouTubeConfig.getApiKey(),mOnInitializedListener);
+//                 break;
+//             case R.id.play_button_3:
+//                 videoStr = "KEYSO-Tc2Go";
+//                 Log.d(TAG,"onClick: Initializing.");
+//                 mYouTubePlayerView1.initialize(YouTubeConfig.getApiKey(),mOnInitializedListener);
+//                 break;
+//             case R.id.play_button_1:
+//                 Log.d(TAG,"onClick: Initializing.");
+//                 mYouTubePlayerView1.initialize(YouTubeConfig.getApiKey(),mOnInitializedListener);
+//                 break;
+//         }
+//     }
  }
