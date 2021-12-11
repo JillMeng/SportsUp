c2cfa5538fec9616e6377053f6d1d106ada1d1ea
Mian Huang <huangmian.huang@alibaba-inc.com>
2021-12-12 03:44:24 +0800

    permission update

--- app/src/main/java/mxh810/com/sportsup/utils/PermissionUtil.java
+++ app/src/main/java/mxh810/com/sportsup/utils/PermissionUtil.java
@@ -6,7 +6,8 @@ public class PermissionUtil {
     public static final String[] PERMISSIONS = {
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.CAMERA
+            Manifest.permission.CAMERA,
+            Manifest.permission.ACCESS_FINE_LOCATION
     };
 
     public static final String[] CAMERA_PERMISSION = {
@@ -19,5 +20,9 @@ public class PermissionUtil {
 
     public static final String[] READ_STORAGE_PERMISSION = {
             Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+    public static final String[] READ_LOCATION_PERMISSION = {
+            Manifest.permission.ACCESS_FINE_LOCATION
     };
 }
